754931f254ae99692ac8e54e78ef9518e8c837f1
AndreaScorza <atlantas95@gmail.com>
2022-11-18 10:30:17 +0100

    Added dynamic loading of nft collection

--- docs/collection.js
+++ docs/collection.js
@@ -1,20 +1,20 @@
+import myJson from './properties/properties.js';
 const el = document.getElementById('collection');
+let text = "<ul class = 'grid'>";
 
-var text = "<ul class = 'grid'>";
-for (var i =0;i<6;i++)
-{
 
+Object.values(myJson).forEach(val => {
   text = text + "<li class='id5 id13'>" +
     "<div class='nft__item'>" +
-      "<div class='img_holder'>" +
-        "<img src='https://ipfs.krazykobe.com/ipfs/QmXzrjBM1QLKUQtzR3iDWoWFptCCWgvzCL7K36souWeTwy/173.jpg' alt=''>" +
-										"<a href='nft-single.html' class='full_link'></a>" +
-									"</div>" +
-									"<div class='title_holder'>" +
-                    "<h3 class='fn_title'><a href='nft-single.html'>Meta Legends #4578</a></h3>" +
-                  "</div>" +
-      "</div>"
+    "<div class='img_holder'>" +
+    "<img src='https://ipfs.krazykobe.com/ipfs/QmXzrjBM1QLKUQtzR3iDWoWFptCCWgvzCL7K36souWeTwy/"+ val.name + ".jpg' alt=''>" +
+    "<a href='nft-single.html' class='full_link'></a>" +
+    "</div>" +
+    "<div class='title_holder'>" +
+    "<h3 class='fn_title'><a href='nft-single.html'>Meta Legends #4578</a></h3>" +
+    "</div>" +
+    "</div>"
+});
 
-}
 text = text +'</ul>';
 el.innerHTML = text;
