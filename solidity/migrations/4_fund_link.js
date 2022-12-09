7b174816955afe8d863afba42f0fea7f3a5ee8ba
AndreaScorza <atlantas95@gmail.com>
2022-12-09 14:25:07 +0100

    Added solidity smart contract

---
+++ solidity/migrations/4_fund_link.js
@@ -0,0 +1,18 @@
+const NFT = artifacts.require("./NFT.sol");
+const allConfigs = require("../config.json");
+const Web3 = require("web3");
+const linkABI = require("../abi/link.abi");
+
+module.exports = async function(deployer, network) {
+  const config = allConfigs[network.replace(/-fork$/, '')] || allConfigs.default;
+  if (!config.verification) return;
+
+  const nft = await NFT.deployed();
+  const linkToken = new web3.eth.Contract(linkABI, config.token);
+  const ownerAddress = await nft.owner();
+
+
+  await linkToken.methods.transfer(nft.address, Web3.utils.toBN(config.verification.fee).muln(10))
+    .send({from: ownerAddress});
+
+};
